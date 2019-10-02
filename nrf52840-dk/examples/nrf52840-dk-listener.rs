b820a1ff1ad6a8df77fbe168f96275cd74d2afa5
Erik Svensson <erik.public@gmail.com>
2019-10-02 22:59:18 +0200

    Example fixes and cleanup
     - Fixed the listerner example for nRF52840-DK and nRF52840-MDK
     - Deleted the beacon examples

--- nrf52840-dk/examples/nrf52840-dk-listener.rs
+++ nrf52840-dk/examples/nrf52840-dk-listener.rs
@@ -2,9 +2,10 @@
 #![no_std]
 
 #[allow(unused_imports)]
-use panic_semihosting;
+use panic_itm;
 
-use cortex_m_semihosting::hprintln;
+use cortex_m::{iprintln, peripheral::ITM};
+
 use rtfm::app;
 
 use bbqueue::{self, bbq, BBQueue};
@@ -14,21 +15,19 @@ use nrf52840_hal::{clocks, gpio, prelude::*, uarte};
 use nrf52840_pac as pac;
 
 use esercom;
-
 use nrf52_radio_802154::radio::{Radio, MAX_PACKET_LENGHT};
 
 #[app(device = nrf52840_pac)]
 const APP: () = {
-    static mut LED_1: gpio::Pin<gpio::Output<gpio::PushPull>> = ();
-    static mut LED_2: gpio::Pin<gpio::Output<gpio::PushPull>> = ();
     static mut RADIO: Radio = ();
+    static mut ITM: ITM = ();
     static mut UARTE: uarte::Uarte<pac::UARTE0> = ();
     static mut RX_PRODUCER: bbqueue::Producer = ();
     static mut RX_CONSUMER: bbqueue::Consumer = ();
 
     #[init]
     fn init() {
-        let p0 = device.P0.split();
+        let pins = device.P0.split();
         // Configure to use external clocks, and start them
         let _clocks = device
             .CLOCK
@@ -39,16 +38,23 @@ const APP: () = {
 
         let uarte0 = device.UARTE0.constrain(
             uarte::Pins {
-                txd: p0.p0_20.into_push_pull_output(gpio::Level::High).degrade(),
-                rxd: p0.p0_19.into_floating_input().degrade(),
-                cts: None,
-                rts: None,
+                txd: pins
+                    .p0_06
+                    .into_push_pull_output(gpio::Level::High)
+                    .degrade(),
+                rxd: pins.p0_08.into_floating_input().degrade(),
+                cts: Some(pins.p0_07.into_floating_input().degrade()),
+                rts: Some(
+                    pins.p0_05
+                        .into_push_pull_output(gpio::Level::High)
+                        .degrade(),
+                ),
             },
             uarte::Parity::EXCLUDED,
             uarte::Baudrate::BAUD115200,
         );
 
-        let bb_queue = bbq![MAX_PACKET_LENGHT * 16].unwrap();
+        let bb_queue = bbq![MAX_PACKET_LENGHT * 32].unwrap();
         let (q_producer, q_consumer) = bb_queue.split();
 
         let mut radio = Radio::new(device.RADIO);
@@ -56,28 +62,23 @@ const APP: () = {
         radio.set_transmission_power(8);
         radio.receive_prepare();
 
-        LED_1 = p0.p0_24.degrade().into_push_pull_output(gpio::Level::Low);
-        LED_2 = p0.p0_23.degrade().into_push_pull_output(gpio::Level::High);
         RADIO = radio;
         UARTE = uarte0;
+        ITM = core.ITM;
         RX_PRODUCER = q_producer;
         RX_CONSUMER = q_consumer;
     }
 
-    #[interrupt(resources = [LED_1, LED_2, RADIO, RX_PRODUCER],)]
+    #[interrupt(resources = [RADIO, RX_PRODUCER],)]
     fn RADIO() {
         let radio = resources.RADIO;
         let queue = resources.RX_PRODUCER;
-
-        (*resources.LED_1).set_high();
-        (*resources.LED_2).set_high();
 
         match queue.grant(MAX_PACKET_LENGHT) {
             Ok(mut grant) => {
                 let packet_len = radio.receive_slice(grant.buf());
                 if packet_len > 0 {
                     queue.commit(packet_len, grant);
-                    (*resources.LED_2).set_low();
                 } else {
                     queue.commit(0, grant);
                 }
@@ -86,16 +87,18 @@ const APP: () = {
                 // Drop package
                 let mut buffer = [0u8; MAX_PACKET_LENGHT];
                 radio.receive(&mut buffer);
-                hprintln!("Failed to queue packet").unwrap();
             }
         }
     }
 
-    #[idle(resources = [RX_CONSUMER, UARTE])]
+    #[idle(resources = [RX_CONSUMER, UARTE, ITM])]
     fn idle() -> ! {
         let mut host_packet = [0u8; MAX_PACKET_LENGHT * 2];
         let queue = resources.RX_CONSUMER;
         let uarte = resources.UARTE;
+        let itm_port = &mut resources.ITM.stim[0];
+
+        iprintln!(itm_port, "~ listening ~");
 
         loop {
             if let Ok(grant) = queue.read() {
@@ -109,7 +112,7 @@ const APP: () = {
                         uarte.write(&host_packet[..written]).unwrap();
                     }
                     Err(_) => {
-                        hprintln!("Failed to encode packet").unwrap();
+                        iprintln!(itm_port, "Failed to encode packet");
                     }
                 }
                 queue.release(packet_length, grant);
